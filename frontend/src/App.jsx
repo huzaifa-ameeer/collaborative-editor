7c8ed1c34579fd79f10a93b94de83a268b190961
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-19 10:05:12 +0500

    setting up the editor

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Editor} from "@monaco-editor/react"
+
+const App = () => {
+  return (
+    <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
+      <aside className='h-full w-1/4 bg-amber-50 rounded-lg'></aside>
+      <section className='w-3/4 bg-neutral-800 overflow-hidden rounded-lg'>
+      
+      <Editor 
+      height="100%"
+      defaultLanguage='javascript'
+      defaultValue='//start from here ...'
+      theme='vs-dark'
+      />
+      
+      </section>
+    </main>
+  )
+}
+
+export default App
