3bf5496bcf44991d4ce36e762d0e6365574bdd1a
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-19 17:10:11 +0500

    setting up yjs in react/client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import {Editor} from "@monaco-editor/react"
+import React, { useMemo, useRef } from "react";
+import { Editor } from "@monaco-editor/react";
+import { MonacoBinding } from "y-monaco";
+import * as Y from "yjs";
+import {SocketIOProvider} from "y-socket.io"
 
 const App = () => {
+
+  const editorRef = useRef(null);
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+
+  const handleMount = (editor)=>{
+    editorRef.current = editor
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect: true
+    })
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  }
+
   return (
-    <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
-      <aside className='h-full w-1/4 bg-amber-50 rounded-lg'></aside>
-      <section className='w-3/4 bg-neutral-800 overflow-hidden rounded-lg'>
-      
-      <Editor 
-      height="100%"
-      defaultLanguage='javascript'
-      defaultValue='//start from here ...'
-      theme='vs-dark'
-      />
-      
+    <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className="w-3/4 bg-neutral-800 overflow-hidden rounded-lg">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="//start from here ..."
+          theme="vs-dark"
+          onMount={handleMount}
+        />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
